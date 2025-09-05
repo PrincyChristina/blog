ee0b057ca1c110c5d75eebf56987706e13d396e5
PrincyChristina <2312079@nec.edu.in>
2025-09-05 15:23:06 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+export default function App(){
+    return(
+    <><h2>Humanity</h2><p>This article is about Hitler.He was a "Dictator" and lead the young generation in a rather inhumane path.His ideals made talented doctors,engineers,teachers not only choose the bad ways but also made them kill infants and follow gore actions.This made us to think that being humane is more important and the education must lead the humans to humanity not away from it at any cost</p></>
+    );
+
+}
